4b8963693f61743dd9eaba9cf8c752564cdb9aec
yilma0110 <ruizelizaldeyilma@gmail.com>
2024-07-02 23:14:13 -0700

    Modal Clientes
    -Crea el modal que se abrirá al seleccionar el botón para editar la información de un cliente

--- workOrdersInterfaz/js/clientes.js
+++ workOrdersInterfaz/js/clientes.js
@@ -1,6 +1,7 @@
 let UrlApi = "http://localhost:8080/api/";
 let dataTable;
 let dataTableIsInitialized;
+const inputs = document.querySelectorAll('input');
 
 const initDataTable = async () =>{
     if(dataTableIsInitialized){
@@ -36,25 +37,27 @@ async function listarClientes(){
             },
             mode:'cors'
         };
-        /*const resp = await fetch(UrlApi + "clientes", options);
+        const resp = await fetch(UrlApi + "clientes", options);
         const data = await resp.json();
-        console.log(data);*/
-        let content = ``;
 
-        content += `<tr>
-                        <td>1</td>
-                        <td>Yilma Lizbeth Ruiz Elizalde</td>
-                        <td>6682717811</td>
-                        <td>ylrelizalde4@gmail.com</td>
-                        <td>Ayer</td>
-                        <td>Hoy</td>
-                        <td> <i class="fa-solid fa-circle-check" style="color:green"> </i> </td>
+        let content = ``;
+        data.forEach(element => {
+            content += `<tr>
+                        <td>${element.IdCliente}</td>
+                        <td>${element.Nombre}</td>
+                        <td>${element.Apellido1}</td>
+                        <td>${element.Apellido2}</td>
+                        <td>${element.Telefono}</td>
+                        <td>${element.Email}</td>
+                        <td>${element.CreatedAt}</td>
+                        <td>${element.UpdatedAt}</td>
+                        <td>${element.Estatus == '1'? element.Estatus = 'Activo' : element.Estatus = 'Inactivo' } </td>
                         <td> 
-                            <button class="btn btn-sm btn-primary"> <i class="fa-solid fa-pencil"></i> </button>
-                            <button class="btn btn-sm btn-warning"> <i class="fa-solid fa-ban"></i> </button>
+                            <button class="btn btn-sm btn-warning" data-bs-toggle="modal" data-bs-target="#mi-modal" > <i class="fa-solid fa-pencil"></i> </button>
                             <button class="btn btn-sm btn-danger"> <i class="fa-solid fa-trash-can"></i> </button>
                         </td>
                     </tr>`;
+        });
 
         tableBody_clientes.innerHTML = content;
     }catch(ex){
@@ -67,3 +70,24 @@ window.addEventListener("load", async()=>{
     await initDataTable();
 
 });
+
+dataTable_clientes.addEventListener("click", (e)=>{
+    e.stopPropagation();
+    console.log(inputs)
+    //toma los datos de la fila donde se encuentra el botón al que se le dio click
+    let clienteSelected = (e.target.parentElement.parentElement.children);
+    btnModificar(clienteSelected)
+
+});
+
+const btnModificar = (data) =>{
+    console.log(data);
+    //Llena el formulario del modal con la información del cliente seleccionado
+    id.innerHTML = data[0].textContent;
+    nombre.value = data[1].textContent;
+    apellido1.value = data[2].textContent;
+    apellido2.value = data[3].textContent;
+    telefono.value = data[4].textContent;
+    email.value = data[5].textContent;
+    data[8].innerText == 'Activo' ? estatus.value = 1 : estatus.value = 2
+}
